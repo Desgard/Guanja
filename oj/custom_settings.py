28838b753044bcecfda98e468e297eae82c6d742
Desgard_Duan <desgard_duan@foxmail.com>
2016-11-03 07:55:26 +0800

    update custom_setting

---
+++ oj/custom_settings.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+import os
+
+
+WEBSITE_INFO = {"website_name": u"Guanja",
+                "website_name_shortcut": u"Guanja",
+                "website_footer": u"Guanja",
+                # url结尾没有/
+                "url": u"http://oj.desgard.com"}
+
+
+SMTP_CONFIG = {"smtp_server": "smtp.domain.com",
+               "email": "noreply@domain.com",
+               "password": "your_password",
+               "tls": False}
+
+
+# 是否显示所有人的提交, False就只显示自己的
+SHOW_ALL_SUBMISSIONS_LIST = False
+
+
+# please set your own SECRET_KEY to a long random string
+SECRET_KEY = "Desgard_Duan"
+
+
